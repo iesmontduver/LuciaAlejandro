0c0281a719021b0f04ce04ec3291ee9d141c73cd
lucconlur <lucconlur@INF1-PC12>
2024-02-14 13:31:11 +0100

    he fet escuderia!!!!!!!!!!!!!!!!!!!1

--- LaFormula1_LUA/src/main/java/ies/montduver/laformula1/Escuderia.java
+++ LaFormula1_LUA/src/main/java/ies/montduver/laformula1/Escuderia.java
@@ -9,5 +9,39 @@ package ies.montduver.laformula1;
  * @author lucconlur
  */
 public class Escuderia {
+    String nom;
+    double pressupost;
+    int punts;
+
+    public Escuderia(String nom, double pressupost, int punts) {
+        this.nom = nom;
+        this.pressupost = pressupost;
+        this.punts = punts;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public double getPressupost() {
+        return pressupost;
+    }
+
+    public void setPressupost(double pressupost) {
+        this.pressupost = pressupost;
+    }
+
+    public int getPunts() {
+        return punts;
+    }
+
+    public void setPunts(int punts) {
+        this.punts = punts;
+    }
+    
     
 }
